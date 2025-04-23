7e07a6b2ff717fbb4d8cbbfb2f7426b75ae6a624
user <user@DESKTOP-EQPI4BB>
2025-04-23 13:46:03 +0900

    初回コミット

---
+++ OvertimeControl/src/main/java/overtime_control/OvertimeControlApplication.java
@@ -0,0 +1,13 @@
+package overtime_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertimeControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvertimeControlApplication.class, args);
+	}
+
+}
